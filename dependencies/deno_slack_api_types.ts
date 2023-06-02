59c3e812536cb73a8de16ca8312ac1d8d5b7e00f
Kazuhiro Sera <seratch@gmail.com>
2023-06-03 08:55:39 +0900

    Upgrade dependencies

--- dependencies/deno_slack_api_types.ts
+++ dependencies/deno_slack_api_types.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/deno_slack_api@2.0.1/types.ts";
+export * from "https://deno.land/x/deno_slack_api@2.1.1/types.ts";
--- dependencies/deno_slack_sdk.ts
+++ dependencies/deno_slack_sdk.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/deno_slack_sdk@2.1.0/mod.ts";
+export * from "https://deno.land/x/deno_slack_sdk@2.1.3/mod.ts";
