93a3f1472cd5effc00925b44f651c911b846e713
Kazuhiro Sera <seratch@gmail.com>
2024-01-16 15:39:35 +0900

    Update deps

--- dependencies/logger.ts
+++ dependencies/logger.ts
@@ -1,2 +1,2 @@
-export { ConsoleHandler } from "https://deno.land/std@0.211.0/log/handlers.ts";
-export * from "https://deno.land/std@0.211.0/log/mod.ts";
+export { ConsoleHandler } from "https://deno.land/std@0.212.0/log/handlers.ts";
+export * from "https://deno.land/std@0.212.0/log/mod.ts";
