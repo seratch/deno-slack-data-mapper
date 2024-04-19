dd43a983719ccbcbf432e62d299b62605167cb62
Kazuhiro Sera <seratch@gmail.com>
2024-04-19 15:58:48 +0900

    Upgrade dependencies

--- dependencies/logger.ts
+++ dependencies/logger.ts
@@ -1,2 +1,2 @@
-export { ConsoleHandler } from "https://deno.land/std@0.221.0/log/console_handler.ts";
-export * from "https://deno.land/std@0.221.0/log/mod.ts";
+export { ConsoleHandler } from "https://deno.land/std@0.223.0/log/console_handler.ts";
+export * from "https://deno.land/std@0.223.0/log/mod.ts";
