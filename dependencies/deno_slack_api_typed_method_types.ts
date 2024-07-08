2f1724c1a59d677041248ba0c49e92dea5d1ffa2
Kazuhiro Sera <seratch@gmail.com>
2024-07-08 10:21:27 +0900

    Upgrade dependencies

--- dependencies/deno_slack_api_typed_method_types.ts
+++ dependencies/deno_slack_api_typed_method_types.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/deno_slack_api@2.6.0/typed-method-types/apps.ts";
+export * from "https://deno.land/x/deno_slack_api@2.7.0/typed-method-types/apps.ts";
--- dependencies/deno_slack_sdk.ts
+++ dependencies/deno_slack_sdk.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/deno_slack_sdk@2.13.0/mod.ts";
+export * from "https://deno.land/x/deno_slack_sdk@2.14.0/mod.ts";
